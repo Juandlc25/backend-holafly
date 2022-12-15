deb4ebbbc3473a72ce6ef3d271a193f467ba2dfd
JuandlcJrDev <jdelacruz@hellobuild.co>
2022-12-15 12:53:28 -0500

    feat (getPeople): Creada funcion de consulta a la BD y si no hay nada en la BD consulta a la api de swapi

--- src/server/endpoints/swapiEndpoints.js
+++ src/server/endpoints/swapiEndpoints.js
@@ -1,29 +1,54 @@
-
+const fetch = require("node-fetch");
 
 const applySwapiEndpoints = (server, app) => {
+  server.get("/hfswapi/test", async (req, res) => {
+    const data = await app.swapiFunctions.genericRequest(
+      "https://swapi.dev/api/",
+      "GET",
+      null,
+      true
+    );
+    res.send(data);
+  });
 
-    server.get('/hfswapi/test', async (req, res) => {
-        const data = await app.swapiFunctions.genericRequest('https://swapi.dev/api/', 'GET', null, true);
-        res.send(data);
-    });
+  server.get("/hfswapi/getPeople/:id", async (req, res) => {
+    const id = req.params.id;
+    const data = await app.db.swPeople.findByPk(id);
+    const options = {
+      method: "GET",
+    };
+    if (data) {
+      res.send(data);
+    } else {
+      const response = await fetch(
+        `https://swapi.dev/api/people/${id}`,
+        options
+      );
+      const { name, height, mass, homeworld } = await response.json();
+      const homeworldRequest = await fetch(homeworld, options);
+      const { name: homeworld_name } = await homeworldRequest.json();
+      res.send({
+        name,
+        mass,
+        height,
+        homeworld_name,
+        homeworld_id: homeworld.replace("https://swapi.dev/api", ""),
+      });
+    }
+  });
 
-    server.get('/hfswapi/getPeople/:id', async (req, res) => {
-        res.sendStatus(501);
-    });
+  server.get("/hfswapi/getPlanet/:id", async (req, res) => {
+    res.sendStatus(501);
+  });
 
-    server.get('/hfswapi/getPlanet/:id', async (req, res) => {
-        res.sendStatus(501);
-    });
+  server.get("/hfswapi/getWeightOnPlanetRandom", async (req, res) => {
+    res.sendStatus(501);
+  });
 
-    server.get('/hfswapi/getWeightOnPlanetRandom', async (req, res) => {
-        res.sendStatus(501);
-    });
-
-    server.get('/hfswapi/getLogs',async (req, res) => {
-        const data = await app.db.logging.findAll();
-        res.send(data);
-    });
-
-}
+  server.get("/hfswapi/getLogs", async (req, res) => {
+    const data = await app.db.logging.findAll();
+    res.send(data);
+  });
+};
 
 module.exports = applySwapiEndpoints;
